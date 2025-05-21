2bcb3575682b2bedadedd075bff62666a7271817
Gildo Junior <110541742+gildofjunior@users.noreply.github.com>
2025-05-21 16:26:52 -0300

    Refactor autopairs

--- nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,37 +1,30 @@
 return {
   "windwp/nvim-autopairs",
-  event = { "InsertEnter" },
-  dependencies = {
-    "hrsh7th/nvim-cmp",
-  },
-  config = function()
-    -- import nvim-autopairs
+  event = "InsertEnter",
+  dependencies = { "hrsh7th/nvim-cmp" },
+  opts = function()
     local autopairs = require("nvim-autopairs")
+    local ts_conds = require("nvim-autopairs.ts-conds")
+    local Rule = require("nvim-autopairs.rule")
 
-    -- configure autopairs
     autopairs.setup({
-      check_ts = true, -- enable treesitter
+      check_ts = true,
       ts_config = {
-        lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-        javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-        java = false, -- don't check treesitter on java
+        lua = { "string" },
+        javascript = { "template_string" },
+        java = false,
       },
     })
 
-    local ts_conds = require("nvim-autopairs.ts-conds")
-    local Rule = require("nvim-autopairs.rule")
     autopairs.add_rules({
-      Rule("%", "%", "lua").with_pair(ts_conds.is_ts_node({ "string", "comment" })),
-      Rule("$", "$", "lua").with_pair(ts_conds.is_not_ts_node({ "function" })),
+      Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({ "string", "comment" })),
+      Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
     })
-
-    -- import nvim-autopairs completion functionality
+  end,
+  config = function(_, _)
+    -- integração com nvim-cmp
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-    -- import nvim-cmp plugin (completions plugin)
     local cmp = require("cmp")
-
-    -- make autopairs and completion work together
     cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
   end,
 }
