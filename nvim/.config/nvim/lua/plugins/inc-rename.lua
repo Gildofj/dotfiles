9b0c50ef2b6b4c281e8875a1e12a737bc311aab3
Gildo Junior <110541742+gildofjunior@users.noreply.github.com>
2025-05-21 16:26:37 -0300

    Fix inc rename conflict command

--- nvim/.config/nvim/lua/plugins/inc-rename.lua
+++ nvim/.config/nvim/lua/plugins/inc-rename.lua
@@ -3,6 +3,6 @@ return {
   config = function()
     require("inc_rename").setup()
 
-    vim.keymap.set("n", "<leader>rn", ":IncRename ")
+    vim.keymap.set("n", "<leader>ir", ":IncRename ")
   end,
 }
