1818d01f4750d7d6823ddb5984fb31a75ce29820
Gildo Junior <110541742+gildofjunior@users.noreply.github.com>
2025-05-23 16:44:35 -0300

    Fix alpha nvim buttons

--- nvim/.config/nvim/lua/plugins/alpha-nvim.lua
+++ nvim/.config/nvim/lua/plugins/alpha-nvim.lua
@@ -34,11 +34,11 @@ return {
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("CTRL + b", "  > Open file explorer", "<cmd>:Neotree filesystem reveal left<CR>"),
+      dashboard.button("CTRL + SHIFT + b", "  > Close file explorer", "<cmd>:Neotree action=close<CR>"),
+      dashboard.button("CTRL + p", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
       dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-      dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+      dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
     }
 
     _Gopts = {
