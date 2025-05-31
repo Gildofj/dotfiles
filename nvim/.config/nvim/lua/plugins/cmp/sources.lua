57aa99d7a1e145dff7d12599dfc3dd906e465991
Gildo Junior <110541742+gildofjunior@users.noreply.github.com>
2025-05-31 12:58:17 -0300

    Change cmp sources

--- nvim/.config/nvim/lua/plugins/cmp/sources.lua
+++ nvim/.config/nvim/lua/plugins/cmp/sources.lua
@@ -5,8 +5,9 @@ return cmp.config.sources({
   { name = "nvim_lsp" }, -- LSP suggestions,
   { name = "nvim_lua" }, -- lua autocompletion,
   { name = "luasnip" }, -- snippets
+  { name = "path" }, -- file system paths
+}, {
   { name = "buffer" }, -- text within current buffer
   { name = "calc" }, -- calc results,
   { name = "emoji" }, -- emojis,
-  { name = "path" }, -- file system paths
 })
