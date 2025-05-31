d1302d9cc6803ddcf6474f893a9cba8c9d9af560
Gildo Junior <110541742+gildofjunior@users.noreply.github.com>
2025-05-31 12:52:58 -0300

    Change catppuccin configs

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -2,9 +2,46 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
+  opts = {
+    integrations = {
+      fidget = true,
+      gitgraph = true,
+      harpoon = true,
+      alpha = true,
+      cmp = true,
+      dashboard = true,
+      fzf = true,
+      gitsigns = true,
+      headlines = true,
+      illuminate = true,
+      indent_blankline = { enabled = true },
+      leap = true,
+      lsp_trouble = true,
+      mason = true,
+      markdown = true,
+      native_lsp = {
+        enabled = true,
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+      navic = { enabled = true, custom_bg = "lualine" },
+      neotest = true,
+      neotree = true,
+      semantic_tokens = true,
+      telescope = true,
+      treesitter = true,
+      treesitter_context = true,
+      which_key = true,
+    },
+  },
   config = function()
-    require("catppuccin").setup()
-    vim.cmd.colorscheme("catppuccin-macchiato")
     -- vim.cmd.colorscheme("catppuccin-latte")
+    -- vim.cmd.colorscheme("catppuccin-frape")
+    -- vim.cmd.colorscheme("catppuccin-macchiato")
+    vim.cmd.colorscheme("catppuccin-mocha")
   end,
 }
