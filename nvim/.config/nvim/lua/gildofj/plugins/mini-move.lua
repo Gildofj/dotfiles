0ca55bb0d064c476948732d86e55cc1c62ee378f
Gildo Junior <110541742+gildofjunior@users.noreply.github.com>
2025-06-02 21:33:42 -0300

    Add mini.move

---
+++ nvim/.config/nvim/lua/gildofj/plugins/mini-move.lua
@@ -0,0 +1,20 @@
+return {
+  "echasnovski/mini.move",
+  event = "VeryLazy",
+  version = "*",
+  opts = {
+    mappings = {
+      -- Mapeia Alt+h/j/k/l para mover em todas as direções
+      left = "<M-h>",
+      right = "<M-l>",
+      down = "<M-j>",
+      up = "<M-k>",
+
+      -- Também funciona em modo visual
+      line_left = "<M-h>",
+      line_right = "<M-l>",
+      line_down = "<M-j>",
+      line_up = "<M-k>",
+    },
+  },
+}
