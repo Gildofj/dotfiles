a4bb10f4f9f23fd34741cd28ad711b212126e919
Gildo Junior <110541742+gildofjunior@users.noreply.github.com>
2025-06-05 13:59:24 -0300

    Implement powershell autocomplete when in windows

--- nvim/.config/nvim/lua/gildofj/plugins/lsp/config/ensure_installed_servers.lua
+++ nvim/.config/nvim/lua/gildofj/plugins/lsp/config/ensure_installed_servers.lua
@@ -1,1 +1,8 @@
-return { "lua_ls", "ts_ls", "cssmodules_ls", "emmet_ls", "html", "jsonls", "tailwindcss", "astro" }
+---@type string[]
+local M = { "lua_ls", "ts_ls", "cssmodules_ls", "emmet_ls", "html", "jsonls", "tailwindcss", "astro" }
+
+if Utils.is_win() then
+  table.insert(M, "powershell_es")
+end
+
+return M
---
+++ nvim/.config/nvim/lua/gildofj/plugins/lsp/config/handlers/powershell_es.lua
@@ -0,0 +1,4 @@
+return {
+  bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
+  filetype = { "ps1", "psm1" },
+}
