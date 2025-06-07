230187c9a40fcbe6eb00683969d85c769166ea2f
Gildo Junior <110541742+gildofjunior@users.noreply.github.com>
2025-06-07 19:47:11 -0300

    replace from spectre to grug-far

--- nvim/.config/nvim/lua/gildofj/plugins/spectre.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-pack/nvim-spectre",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = true,
-  keys = {
-    -- stylua: ignore
-    { "<leader>sr", function() require("spectre").open() end, desc = "Search & Replace (Spectre)" },
-  },
-}
