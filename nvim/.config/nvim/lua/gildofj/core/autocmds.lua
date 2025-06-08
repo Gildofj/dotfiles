755be41f196957633d9b73a71b016aabd8746a6a
Gildo Junior <110541742+gildofjunior@users.noreply.github.com>
2025-06-08 07:29:10 -0300

    fix deprecated functions

--- nvim/.config/nvim/lua/gildofj/core/autocmds.lua
+++ nvim/.config/nvim/lua/gildofj/core/autocmds.lua
@@ -5,7 +5,7 @@ vim.cmd([[let &t_Ce = "\e[4:0m"]])
 -- Yank highlighting
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
-    vim.highlight.on_yank({
+    vim.hl.on_yank({
       higroup = "IncSearch",
       timeout = 300,
     })
