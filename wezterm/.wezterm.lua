01e05285d63fe0693b37a90aa5c1c1ec425d897c
Gildo Junior <110541742+gildofjunior@users.noreply.github.com>
2026-06-12 17:37:49 -0300

    feat: Add font fallback and auto theme switcher in wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,9 +4,31 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 28
 
-config.font = wezterm.font("Jetbrains Mono")
+config.font = wezterm.font_with_fallback({
+	"FiraCode Nerd Font",
+	"Fira Code",
+	"JetBrains Mono",
+})
 config.font_size = 12
-config.color_scheme = "Catppuccin Mocha"
+
+-- Detecta a aparência do sistema de forma segura (evitando erros no servidor multiplexer)
+local function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+-- Retorna o tema apropriado para a aparência detectada
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
 
 config.window_close_confirmation = "NeverPrompt"
 
